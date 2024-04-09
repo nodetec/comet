5077b29089d241d6e254fe6e6eb211abd995819b
Christian Chiarulli <chris.machine@pm.me>
2024-04-09 18:25:27 -0400

    put strict mode back

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,24 @@
 import React from "react";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { ThemeProvider } from "./components/theme/ThemeProvider";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const queryClient = new QueryClient()
-
-  // <React.StrictMode>
-  //
-  // </React.StrictMode>,
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <QueryClientProvider client={ queryClient }>
-    <ReactQueryDevtools initialIsOpen={false} />
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </ThemeProvider>
     </QueryClientProvider>
+  </React.StrictMode>,
 );
