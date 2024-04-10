e0ee69143689cb144f4ad83163448370eb19e8b6
Christian Chiarulli <chris.machine@pm.me>
2024-04-09 20:24:39 -0400

    muted search icon

--- src/components/notes/SearchNotes.tsx
+++ src/components/notes/SearchNotes.tsx
@@ -5,7 +5,7 @@ import { Input } from "../ui/input";
 export default function SearchNotes() {
   return (
     <div className="flex items-center py-2 pr-4">
-      <MagnifyingGlassIcon className="pointer-events-none relative left-8 top-2.5 -translate-y-1/2 transform h-[1.2rem] w-[1.2rem]" />
+      <MagnifyingGlassIcon className="pointer-events-none relative left-8 top-2.5 text-muted-foreground -translate-y-1/2 transform h-[1.2rem] w-[1.2rem]" />
       <Input placeholder="Search..." className="pl-10 focus-visible:ring-muted-foreground/30" />
     </div>
   );
