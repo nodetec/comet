6a75dae97bf8439ba746f0ca7515606e501849de
Christian Chiarulli <chris.machine@pm.me>
2024-04-14 20:15:14 -0400

    store last active note and tag in local storage and restore

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
+import { type ActiveNote, type Tag } from "~/types";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
-import { ActiveNote, Tag } from "~/types";
 
 interface State {
   activeNote: ActiveNote | undefined;
@@ -11,19 +11,20 @@ interface State {
   setNoteSearch: (noteSearch: string | undefined) => void;
 }
 
+// TODO: partial state storage, leave out search
 export const useGlobalState = create<State>()(
-    // persist(
-      (set) => ({
-        activeNote: undefined,
-        setActiveNote: (note) => set({ activeNote: note }),
-        activeTag: undefined,
-        setActiveTag: (tag) => set({ activeTag: tag }),
-        noteSearch: undefined,
-        setNoteSearch: (noteSearch) => set({ noteSearch: noteSearch }),
-      }),
-      // {
-      //   name: "captains-log-storage",
-      //   storage: createJSONStorage(() => localStorage)
-      // },
-    // ),
+  persist(
+    (set) => ({
+      activeNote: undefined,
+      setActiveNote: (note) => set({ activeNote: note }),
+      activeTag: undefined,
+      setActiveTag: (tag) => set({ activeTag: tag }),
+      noteSearch: undefined,
+      setNoteSearch: (noteSearch) => set({ noteSearch: noteSearch }),
+    }),
+    {
+      name: "captains-log-storage",
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
 );
