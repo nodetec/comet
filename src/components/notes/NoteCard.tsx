3248a9bf81e4c696206e51244b4a45830c79d01c
Christian Chiarulli <chris.machine@pm.me>
2024-04-17 00:46:25 -0400

    underline

--- src/components/notes/NoteCard.tsx
+++ src/components/notes/NoteCard.tsx
@@ -86,7 +86,7 @@ export default function NoteCard({ note }: Props) {
         onContextMenu={handleContextMenu}
         onClick={handleSetActiveNote}
         key={note.id}
-        className={`flex h-full w-full cursor-pointer select-none flex-col gap-y-1 rounded-md p-2 text-sm ${activeNote?.id === note.id && "bg-muted/80"}`}
+        className={`flex h-full w-full cursor-pointer select-none flex-col gap-y-1 rounded-md border-b px-2 pb-3 pt-3 text-sm ${activeNote?.id === note.id && "bg-muted/80"}`}
       >
         <h2 className="select-none font-semibold text-primary">
           {parseTitle(note.content).title}
