eddbf1e469e9323ed27429efdd79230f74cb41f6
Christian Chiarulli <chris.machine@pm.me>
2024-04-26 06:05:19 -0400

    format

--- src/components/notes/NoteFeed.tsx
+++ src/components/notes/NoteFeed.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { listNotes } from "~/api";
