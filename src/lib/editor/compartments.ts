eebd2b8b3c4597a79ade2c46c0d1a18746abd0a7
Jason Chiarulli <jaythecodemonkey.7vk7i@slmail.me>
2024-06-16 16:15:01 -0400

    feat: enable more editor settings #4 (#20)

---
+++ src/lib/editor/compartments.ts
@@ -0,0 +1,11 @@
+import { Compartment } from "@codemirror/state";
+
+const fontSize = new Compartment();
+
+const fontFamily = new Compartment();
+
+const fontWeight = new Compartment();
+
+const lineHeight = new Compartment();
+
+export { fontSize, fontFamily, fontWeight, lineHeight };
