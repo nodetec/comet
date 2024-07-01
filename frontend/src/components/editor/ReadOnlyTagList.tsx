ab700e779b8ca45380fffdf672335a653294e28a
Christian Chiarulli <chris.machine@pm.me>
2024-07-01 06:48:21 -0400

    don't show tag if there are none

--- frontend/src/components/editor/ReadOnlyTagList.tsx
+++ frontend/src/components/editor/ReadOnlyTagList.tsx
@@ -10,20 +10,19 @@ export default function ReadOnlyTagList({ trashNote }: Props) {
   return (
     <div className="w-full border-t py-2 pl-4 pr-2">
       <div className="flex items-center gap-x-2">
-        {trashNote.Tags?.String.split(",").map((tag) => {
-          return (
-            <div>
-              <Badge
-                className="cursor-default select-none whitespace-nowrap rounded-full"
-                variant="secondary"
-              >
-                {tag}
-              </Badge>
-            </div>
-          );
-
-          // return <NoteTag key={tagIndex} note={note} tag={tag} />;
-        })}
+        {trashNote.Tags?.String !== "" &&
+          trashNote.Tags?.String.split(",").map((tag) => {
+            return (
+              <div>
+                <Badge
+                  className="cursor-default select-none whitespace-nowrap rounded-full"
+                  variant="secondary"
+                >
+                  {tag}
+                </Badge>
+              </div>
+            );
+          })}
         <div className="py-[1.125rem]"></div>
       </div>
     </div>
