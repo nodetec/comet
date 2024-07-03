c26bec8857cfd8125df8b142c993d43f3f14d11d
Christian Chiarulli <chris.machine@pm.me>
2024-07-03 09:30:47 -0400

    hover login colors

--- frontend/src/components/sidebar/Login.tsx
+++ frontend/src/components/sidebar/Login.tsx
@@ -1,6 +1,7 @@
 import { GearIcon } from "@radix-ui/react-icons";
 import * as wails from "@wailsio/runtime";
 import { ArrowRightIcon } from "lucide-react";
+
 import { Button } from "../ui/button";
 
 export default function Login() {
@@ -11,16 +12,17 @@ export default function Login() {
   return (
     <div className="flex items-center gap-4 border-t bg-black/10 p-2">
       <div className="flex w-full items-center justify-between">
-        <Button 
+        <Button
           variant="ghost"
-          className="flex items-center gap-x-1 my-0">
-          <p className="text-sm text-muted-foreground/90">Login</p>
-          <ArrowRightIcon className="h-3 w-3 text-muted-foreground/90" />
+          className="my-0 flex items-center gap-x-1 text-muted-foreground hover:bg-background hover:text-white"
+        >
+          <p className="text-sm">Login</p>
+          <ArrowRightIcon className="h-3 w-3" />
         </Button>
 
         <GearIcon
           onClick={handleOpenSettings}
-          className="h-5 w-5 cursor-pointer text-muted-foreground hover:text-foreground"
+          className="mr-3 h-5 w-5 cursor-pointer text-muted-foreground hover:text-foreground"
         />
       </div>
     </div>
