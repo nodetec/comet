1f2d73af79d68b336ef73a8e03379ea9ad888a1b
Christian Chiarulli <chris.machine@pm.me>
2024-07-03 09:31:17 -0400

    less x padding login section

--- frontend/src/components/sidebar/Login.tsx
+++ frontend/src/components/sidebar/Login.tsx
@@ -10,7 +10,7 @@ export default function Login() {
   };
 
   return (
-    <div className="flex items-center gap-4 border-t bg-black/10 p-2">
+    <div className="flex items-center gap-4 border-t bg-black/10 py-2">
       <div className="flex w-full items-center justify-between">
         <Button
           variant="ghost"
