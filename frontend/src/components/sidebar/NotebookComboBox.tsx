0a65bf61e191238de02c6b9d32111276eaecf021
Christian Chiarulli <chris.machine@pm.me>
2024-07-04 15:03:47 -0400

    set tag undefined if not associated with notebook

--- frontend/src/components/sidebar/NotebookComboBox.tsx
+++ frontend/src/components/sidebar/NotebookComboBox.tsx
@@ -38,6 +38,8 @@ export function NotebookComboBox() {
     setFeedType,
     activeNote,
     setActiveNote,
+    activeTag,
+    setActiveTag,
   } = useAppState();
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
@@ -69,7 +71,7 @@ export function NotebookComboBox() {
     queryFn: () => fetchNotebooks(),
   });
 
-  const selectNotebook = (notebookName: string, notebook: Notebook) => {
+  const selectNotebook = async (notebookName: string, notebook: Notebook) => {
     setActiveNotebook(notebook);
     setValue(notebookName);
     setOpen(false);
@@ -83,6 +85,13 @@ export function NotebookComboBox() {
     void queryClient.invalidateQueries({
       queryKey: ["tags"],
     });
+    if (activeTag) {
+      const isTagAssociatedWithNotebook =
+        await NotebookService.CheckTagForNotebook(notebook.ID, activeTag?.ID);
+      if (isTagAssociatedWithNotebook) {
+        setActiveTag(undefined);
+      }
+    }
   };
 
   return (
