a3ea702da6a86f21889233e1375fb69bf19b5480
nicholaschiarulli <nicholasrchiarulli@gmail.com>
2024-07-06 00:27:53 -0400

    first migration

---
+++ db/migrations/20240706035555_add_pinned_to_notes.sql
@@ -0,0 +1,9 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE notes ADD COLUMN pinned BOOLEAN DEFAULT FALSE;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE notes DROP COLUMN pinned;
+-- +goose StatementEnd
