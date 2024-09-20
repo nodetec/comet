3ff0ce3981af7e986a8a362c10aa26bbce56606a
Christian Chiarulli <chris.machine@pm.me>
2024-09-19 21:23:09 -0400

    remove border

--- frontend/src/features/editor/components/CodeMirrorEditor.tsx
+++ frontend/src/features/editor/components/CodeMirrorEditor.tsx
@@ -65,7 +65,7 @@ export const CodeMirrorEditor = () => {
   return (
     <div className="flex h-full w-full overflow-y-auto">
       <div
-        className={`prose prose-zinc h-full w-full max-w-none break-words border border-red-500 pb-4 dark:prose-invert ${editorFullScreen && "py-4"}`}
+        className={`prose prose-zinc h-full w-full max-w-none break-words pb-4 dark:prose-invert ${editorFullScreen && "py-4"}`}
         ref={editorRef}
       ></div>
     </div>
