1d9ee779a9ff21da3026bf9676230acd20e2886d
Christian Chiarulli <chris.machine@pm.me>
2025-03-15 03:18:24 -0400

    auto open profile when clicked

--- src/frontend/features/sidebar/components/SidebarHeader.tsx
+++ src/frontend/features/sidebar/components/SidebarHeader.tsx
@@ -1,12 +1,19 @@
 import { Button } from "~/components/ui/button";
 import { Settings } from "~/features/settings";
+import { useAppState } from "~/store";
 import { Settings2Icon, UserCircleIcon } from "lucide-react";
 
 export function SidebarHeader() {
+  const setSettingsTab = useAppState((state) => state.setSettingsTab);
+
+  function handleClick() {
+    setSettingsTab("profile");
+  }
+
   return (
     <header className="draggable flex justify-end gap-1 px-4 pt-2 pb-4">
       <Settings>
-        <Button type="button" variant="ghost" size="icon">
+        <Button onClick={handleClick} type="button" variant="ghost" size="icon">
           <UserCircleIcon />
         </Button>
       </Settings>
