9c6aaf4bd0b7b205fc78e37386e88ef74ab859b0
Christian Chiarulli <chris.machine@pm.me>
2025-03-15 04:30:00 -0400

    fix publish dialog scroll area

--- src/frontend/features/editor/components/PublishDialog.tsx
+++ src/frontend/features/editor/components/PublishDialog.tsx
@@ -38,18 +38,18 @@ export function PublishDialog() {
           </Button>
         )}
       </DialogTrigger>
-      <DialogContent className="border-accent h-[85%] max-h-[50rem] w-[90%] max-w-[40rem] overflow-hidden border select-none">
-        <ScrollArea className="m-2">
-          <DialogHeader>
-            {note.data?.identifier ? (
-              <DialogTitle>Update Note</DialogTitle>
-            ) : (
-              <DialogTitle>Publish Note</DialogTitle>
-            )}
-            <DialogDescription>Publish to the nostr network.</DialogDescription>
-          </DialogHeader>
+      <DialogContent className="border-accent h-[85%] max-h-[50rem] w-[90%] max-w-[40rem] overflow-hidden overflow-y-scroll border select-none">
+        <DialogHeader>
+          {note.data?.identifier ? (
+            <DialogTitle>Update Note</DialogTitle>
+          ) : (
+            <DialogTitle>Publish Note</DialogTitle>
+          )}
+          <DialogDescription>Publish to the nostr network.</DialogDescription>
+        </DialogHeader>
 
-          <div className="flex flex-col gap-4 my-4">
+        <div className="my-4 flex flex-col gap-4">
+          {previewImage && (
             <div className="border-accent max-w-md rounded-md border">
               <img
                 src={previewImage ? previewImage[1] : ""}
@@ -57,51 +57,51 @@ export function PublishDialog() {
                 className="h-full w-full rounded-md object-contain"
               />
             </div>
+          )}
 
-            <div className="space-y-4">
-              <div>
-                <h3 className="mb-2 font-semibold">Title:</h3>
-                <p className="no-scrollbar bg-accent cursor-default overflow-x-auto rounded-md px-2 py-1">
-                  {note.data?.title}
-                </p>
-              </div>
-              <div>
-                <h3 className="mb-2 font-semibold">Author:</h3>
-                <p className="no-scrollbar bg-accent cursor-default overflow-x-auto rounded-md px-2 py-1">
-                  {keys?.npub}
-                </p>
-              </div>
-              <div>
-                <h3 className="mb-2 font-semibold">Tags:</h3>
-                <div className="flex flex-wrap gap-2">
-                  {note.data?.tags.map((tag, index) => (
-                    <Badge
-                      className="cursor-default"
-                      key={index}
-                      variant="accent"
-                    >
-                      {tag}
-                    </Badge>
-                  ))}
-                </div>
+          <div className="space-y-4">
+            <div>
+              <h3 className="mb-2 font-semibold">Title:</h3>
+              <p className="no-scrollbar bg-accent cursor-default overflow-x-auto rounded-md px-2 py-1">
+                {note.data?.title}
+              </p>
+            </div>
+            <div>
+              <h3 className="mb-2 font-semibold">Author:</h3>
+              <p className="no-scrollbar bg-accent cursor-default overflow-x-auto rounded-md px-2 py-1">
+                {keys?.npub}
+              </p>
+            </div>
+            <div>
+              <h3 className="mb-2 font-semibold">Tags:</h3>
+              <div className="flex flex-wrap gap-2">
+                {note.data?.tags.map((tag, index) => (
+                  <Badge
+                    className="cursor-default"
+                    key={index}
+                    variant="accent"
+                  >
+                    {tag}
+                  </Badge>
+                ))}
               </div>
             </div>
           </div>
+        </div>
 
-          <DialogFooter>
-            <Button variant="outline" onClick={() => setIsOpen(false)}>
-              Cancel
-            </Button>
+        <DialogFooter>
+          <Button variant="outline" onClick={() => setIsOpen(false)}>
+            Cancel
+          </Button>
 
-            <Button
-              onClick={(e) =>
-                handlePublish(e, note.data, keys, () => setIsOpen(false))
-              }
-            >
-              Publish
-            </Button>
-          </DialogFooter>
-        </ScrollArea>
+          <Button
+            onClick={(e) =>
+              handlePublish(e, note.data, keys, () => setIsOpen(false))
+            }
+          >
+            Publish
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
