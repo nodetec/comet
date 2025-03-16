594521ca7a61f6d1acee34ecdc9740091760e0b7
Christian Chiarulli <chris.machine@pm.me>
2025-03-16 07:43:52 -0400

    border accent for dialog

--- src/frontend/components/ui/dialog.tsx
+++ src/frontend/components/ui/dialog.tsx
@@ -56,7 +56,7 @@ function DialogContent({
         data-slot="dialog-content"
         // max-w-[calc(100%-2rem)]
         className={cn(
-          "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 max-w-lg",
+          "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border border-accent p-6 shadow-lg duration-200 max-w-lg",
           className,
         )}
         {...props}
